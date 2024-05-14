2055f32c8585600f069fe75c174a1fd6b7ae31b4
LuisScholze <131001253+LuisScholze@users.noreply.github.com>
2024-05-14 10:59:30 -0300

    njh

--- main.js
+++ main.js
@@ -23,10 +23,10 @@ const tempoObjetivo4 = new Date("2024-12-10T00:00:00");
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 function atualizaCronometro(){
-    document.getElementById("dias0").textContent = calculaTempo(tempos[5])[0];
-    document.getElementById("horas0").textContent = calculaTempo(tempos[4])[1];
-    document.getElementById("min0").textContent = calculaTempo(tempos[4])[2];
-    document.getElementById("seg0").textContent = calculaTempo(tempos[5])[3];
+    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
+    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
+    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
+    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
 
     for (let i=0; i<contadores.length;i++){
        // contadores[i].textContent = calculaTempo(tempos[i]);   
