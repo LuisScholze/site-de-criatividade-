30aa242f61a8348d13f4306425e237ff8650989d
LuisScholze <131001253+LuisScholze@users.noreply.github.com>
2024-05-14 07:18:40 -0300

    14

--- main.js
+++ main.js
@@ -51,5 +51,6 @@ function calculaTempo(tempoObjetivo) {
  
     if (tempoFinal > 0){
         return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-    } else {
-
+    } else 
+    
+}
