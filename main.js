4a3ef9b773a9ac2b8cbe1b9a868651d1559ce562
LuisScholze <131001253+LuisScholze@users.noreply.github.com>
2024-05-14 10:55:48 -0300

    gftr

--- main.js
+++ main.js
@@ -55,6 +55,12 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
+    if (tempoFinal > 0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    } else {
+        return [0,0,0,0];
+    }
+
  
  return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
 }
